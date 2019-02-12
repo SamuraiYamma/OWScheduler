9ed555fc5613782e32f312cb59ebd5be6c319328
terrainbow <allisonjoy413@gmail.com>
2019-02-11 20:31:17 -0500

    fixed the home page to not require /home/ on base url

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'scheduler'
 urlpatterns = [
-    path('home/', views.home), # home page
+    path('', views.home), # home page
     path('players/', views.players), # search for players
     re_path(r'^players/(?P<battlenetID>([A-Za-z]+)#[0-9]{4-5})/', views.playerProfile), # player profile page
     re_path(r'^players/(?P<battlenetID>([A-Za-z]+)#[0-9]{4-5})/account', views.account), # edit player account
